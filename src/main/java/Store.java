166f23d92a3e8d474c42c66be280780d3ce58ae1
pandukakarunasena <panduka.karunasena@gmail.com>
2023-05-14 23:20:34 +0530

    Implement functionality to restock product thread safely

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,11 +1,15 @@
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
 
 public class Store {
 
     private HashMap<String, Product> products = new HashMap<>();
-    private ArrayList<Product> needsToRestocked = new ArrayList<>();
+    private ConcurrentLinkedDeque<Product> restockNeedProducts = new ConcurrentLinkedDeque<>();
+    private Lock lock = new ReentrantLock();
+    private Condition notEmpty = lock.newCondition();
 
     public HashMap<String, Product> getProducts() { return products; }
     public void addProduct(Product product) {
@@ -19,10 +23,36 @@ public class Store {
         return products.get(productName);
     }
 
-    public List getNeedsToRestocked() {
+    public ConcurrentLinkedDeque<Product> getRestockNeedProducts() {
 
-        return needsToRestocked;
+        return restockNeedProducts;
     }
 
+    public void addProductsToRestockList(Product product) {
 
+        lock.lock();
+        try {
+            restockNeedProducts.add(product);
+            notEmpty.signalAll();
+        } finally {
+             lock.unlock();
+        }
+    }
+
+    public void removeProductFromRestockList(Product product) {
+
+        lock.lock();
+        try {
+            while (restockNeedProducts.isEmpty()) {
+                notEmpty.await();
+            }
+
+            restockNeedProducts.remove(product);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+
+    }
 }
