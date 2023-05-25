af833ac60e3fd862f5066ea49b5f318fd7a51fcc
pandukakarunasena <panduka.karunasena@gmail.com>
2023-05-26 00:58:55 +0530

    Refactor store

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,20 +1,22 @@
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.Condition;
 
 public class Store {
 
     private HashMap<String, Product> products = new HashMap<>();
-    private ConcurrentLinkedQueue<Product> restockNeedProducts = new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<Product> restockNeedProducts = new ConcurrentLinkedQueue<>();//do we need this if we satisfy a customer until he gets all the shopping list items.
     private ConcurrentLinkedQueue<Cart> carts  = new ConcurrentLinkedQueue<>();
-    private Lock lock = new ReentrantLock();
-    private Condition notEmpty = lock.newCondition();
 
     public HashMap<String, Product> getProducts() { return products; }
-    public void addProduct(Product product) {
+
+    public void addProductToStore(Product product) {
         products.put(product.getName(), product);
+    }
+
+    public synchronized void addProductsToRestockList(Product product) {
+        if (!restockNeedProducts.contains(product)) {
+            restockNeedProducts.add(product);
+        }
     }
 
     public Product getProduct(String productName) {
@@ -25,34 +27,11 @@ public class Store {
         return restockNeedProducts;
     }
 
+    public void addCartsToCheckoutQueue(Cart cart) {
+        carts.add(cart);
+    }
+
     public ConcurrentLinkedQueue getCheckoutQueue() {
         return carts;
-    }
-
-    public void addProductsToRestockList(Product product) {
-
-        lock.lock();
-        try {
-            restockNeedProducts.add(product);
-        } finally {
-             lock.unlock();
-        }
-    }
-
-    public void removeProductFromRestockList(Product product) {
-
-        lock.lock();
-        try {
-            while (restockNeedProducts.isEmpty()) {
-                notEmpty.await();
-            }
-
-            restockNeedProducts.remove(product);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            lock.unlock();
-        }
-
     }
 }
