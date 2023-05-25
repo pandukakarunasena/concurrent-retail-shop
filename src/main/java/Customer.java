3bee1cb3a96392d4c3b5625fb9fc7855c1caf844
pandukakarunasena <panduka.karunasena@gmail.com>
2023-05-26 00:58:12 +0530

    Revamp customer threads

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -24,41 +24,32 @@ public class Customer implements Runnable{
 
         System.out.println(Thread.currentThread().getName() +" started shopping....");
 
+
         while(!shoppingList.isEmpty()) {
             System.out.println(Thread.currentThread().getName() + " has " + shoppingList.stream().map(ShoppingItem::getName).collect(Collectors.joining(", ")) + " to buy.");
             Iterator<ShoppingItem> shoppingListIterator = shoppingList.iterator();
             while (shoppingListIterator.hasNext()) {
-                ShoppingItem item = shoppingListIterator.next();
-                System.out.println(Thread.currentThread().getName() + " checking for " + item.getQuantity() + " " + item.getName() + " to buy.");
-                Product product = store.getProduct(item.getName());
+                ShoppingItem shoppingitem = shoppingListIterator.next();
+                System.out.println(Thread.currentThread().getName() + " checking for " + shoppingitem.getQuantity() + " " + shoppingitem.getName() + " to buy.");
+                Product product = store.getProduct(shoppingitem.getName());
 
-                synchronized (product) {
-                    if (product != null) {
-                        System.out.println(Thread.currentThread().getName() + " found " + item.getName());
-                        boolean canPurchase = product.canPurchase(item.getQuantity());
-                        if (canPurchase) {
-                            cart.addShoppingItem(item);
-                            System.out.println(Thread.currentThread().getName() + " added " + item.getQuantity() + " " + item.getName() + " to the cart");
-                            shoppingListIterator.remove();
-                            System.out.println(Thread.currentThread().getName() + " removed " + item.getName() + " from shopping list");
-                        } else {
-                            System.out.println(Thread.currentThread().getName() + " no stocks available for adding to cart " + item.getName());
-                            //customers wait until the product gets restocked. until that customers will roam around the store
-                            //should we inform the admins to restock ????
-                            if (!store.getRestockNeedProducts().contains(product)) {
-                                System.out.println(Thread.currentThread().getName() + " notify the admins to restock " + item.getQuantity() + " " + item.getName());
-                                store.addProductsToRestockList(product);
-                                notify();
-                            } else {
-                                System.out.println(Thread.currentThread().getName() + " " + item.getName() + " is already in the need stocks queue");
-                            }
-
-                        }
+                if (product != null) {
+                    System.out.println(Thread.currentThread().getName() + " found " + shoppingitem.getName());
+                    boolean canPurchase = product.canPurchase(shoppingitem.getQuantity());
+                    if (canPurchase) {
+                        cart.addShoppingItem(shoppingitem);
+                        System.out.println(Thread.currentThread().getName() + " added " + shoppingitem.getQuantity() + " " + shoppingitem.getName() + " to the cart");
+                        shoppingListIterator.remove();
+                        System.out.println(Thread.currentThread().getName() + " removed " + shoppingitem.getName() + " from shopping list");
                     } else {
-                        System.out.println(item.getName() + " is not available.");
+                        System.out.println(Thread.currentThread().getName() + " no stocks available for adding to cart " + shoppingitem.getName());
+                        store.addProductsToRestockList(product);
+                        System.out.println(Thread.currentThread().getName() + " notified the admins to restock " + shoppingitem.getQuantity() + " " + shoppingitem.getName());
                     }
-                    System.out.println(Thread.currentThread().getName() + " has left " + shoppingList.stream().map(ShoppingItem::getName).collect(Collectors.joining(", ")) + " to buy.");
+                } else {
+                    System.out.println(shoppingitem.getName() + " is not available.");
                 }
+                System.out.println(Thread.currentThread().getName() + " has left " + shoppingList.stream().map(ShoppingItem::getName).collect(Collectors.joining(", ")) + " to buy.");
             }
         }
 
