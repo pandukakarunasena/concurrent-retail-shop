272c9c9be1aeaee969d781e986797040c3914fa5
pandukakarunasena <panduka.karunasena@gmail.com>
2023-05-29 10:28:20 +0530

    Fix styling issues.

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -24,38 +24,36 @@ public class Customer implements Runnable{
 
         System.out.println(Thread.currentThread().getName() +" started shopping....");
 
-
         while(!shoppingList.isEmpty()) {
-            System.out.println(Thread.currentThread().getName() + " has " + shoppingList.stream().map(ShoppingItem::getName).collect(Collectors.joining(", ")) + " to buy.");
+            System.out.println(Thread.currentThread().getName() + " has "
+                    + shoppingList.stream().map(ShoppingItem::getName).collect(Collectors.joining(", "))
+                    + " to buy.");
             Iterator<ShoppingItem> shoppingListIterator = shoppingList.iterator();
             while (shoppingListIterator.hasNext()) {
                 ShoppingItem shoppingitem = shoppingListIterator.next();
-                System.out.println(Thread.currentThread().getName() + " checking for " + shoppingitem.getQuantity() + " " + shoppingitem.getName() + " to buy.");
                 Product product = store.getProduct(shoppingitem.getName());
 
                 if (product != null) {
-                    System.out.println(Thread.currentThread().getName() + " found " + shoppingitem.getName());
-                    boolean canPurchase = product.canPurchase(shoppingitem.getQuantity());
+                    boolean canPurchase = product.addToCart(shoppingitem.getQuantity());
                     if (canPurchase) {
                         cart.addShoppingItem(shoppingitem);
-                        System.out.println(Thread.currentThread().getName() + " added " + shoppingitem.getQuantity() + " " + shoppingitem.getName() + " to the cart");
                         shoppingListIterator.remove();
-                        System.out.println(Thread.currentThread().getName() + " removed " + shoppingitem.getName() + " from shopping list");
+                        System.out.println(Thread.currentThread().getName() + " removed " + shoppingitem.getName()
+                                + " from shopping list");
                     } else {
-                        System.out.println(Thread.currentThread().getName() + " no stocks available for adding to cart " + shoppingitem.getName());
-                        store.addProductsToRestockList(product);
-                        System.out.println(Thread.currentThread().getName() + " notified the admins to restock " + shoppingitem.getQuantity() + " " + shoppingitem.getName());
+                        System.out.println(Thread.currentThread().getName() + " waiting for restock to add to cart: "
+                                + shoppingitem.getQuantity() + " " + shoppingitem.getName());
                     }
                 } else {
                     System.out.println(shoppingitem.getName() + " is not available.");
                 }
-                System.out.println(Thread.currentThread().getName() + " has left " + shoppingList.stream().map(ShoppingItem::getName).collect(Collectors.joining(", ")) + " to buy.");
+                System.out.println(Thread.currentThread().getName() + " has left "
+                        + shoppingList.stream().map(ShoppingItem::getName).collect(Collectors.joining(", "))
+                        + " to buy.");
             }
         }
 
-        //go to the checkout queue. this is not synced to each thread whoever adds the cart first is checked out first.
-        //and if the stocks are not there the customer will hold the queue until stocks arrives.
-        store.getCheckoutQueue().add(cart);
+        store.addToQueue(this);
         System.out.println(Thread.currentThread().getName() +" is in the checkout queue....");
     }
 }
--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -4,34 +4,21 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class Store {
 
     private HashMap<String, Product> products = new HashMap<>();
-    private ConcurrentLinkedQueue<Product> restockNeedProducts = new ConcurrentLinkedQueue<>();//do we need this if we satisfy a customer until he gets all the shopping list items.
-    private ConcurrentLinkedQueue<Cart> carts  = new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<Customer> customerQueue = new ConcurrentLinkedQueue();
 
     public HashMap<String, Product> getProducts() { return products; }
 
-    public void addProductToStore(Product product) {
-        products.put(product.getName(), product);
-    }
-
-    public synchronized void addProductsToRestockList(Product product) {
-        if (!restockNeedProducts.contains(product)) {
-            restockNeedProducts.add(product);
-        }
-    }
+    public void addProductToStore(Product product) { products.put(product.getName(), product);}
 
     public Product getProduct(String productName) {
         return products.get(productName);
     }
 
-    public ConcurrentLinkedQueue<Product> getRestockNeedProducts() {
-        return restockNeedProducts;
+    public void addToQueue(Customer customer) {
+        customerQueue.add(customer);
     }
 
-    public void addCartsToCheckoutQueue(Cart cart) {
-        carts.add(cart);
-    }
-
-    public ConcurrentLinkedQueue getCheckoutQueue() {
-        return carts;
+    public ConcurrentLinkedQueue<Customer> getCustomerQueue() {
+        return customerQueue;
     }
 }
